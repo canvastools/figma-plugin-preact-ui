778bdb8d1d7cbdeb8dced068272ab8c85a7a6f70
Aleksei Sushkov <903434@gmail.com>
2024-08-18 14:52:24 +0200

    Updates

---
+++ assets/Loader.stories-E_JwdTrp.js
@@ -0,0 +1,3 @@
+import{_ as e}from"./preact.module-J7DzsJUt.js";const t=()=>e("div",{className:"Loader"},e("div",{className:"Loader__spinner"})),m={title:"Components/State/Loader",component:t},r={args:{}};var a,o,s;r.parameters={...r.parameters,docs:{...(a=r.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  args: {}
+}`,...(s=(o=r.parameters)==null?void 0:o.docs)==null?void 0:s.source}}};const c=["Primary"];export{r as Primary,c as __namedExportsOrder,m as default};
---
+++ assets/Test.stories-CnRaXExi.js
@@ -0,0 +1,5 @@
+import{_ as a}from"./preact.module-J7DzsJUt.js";const n=t=>a("div",{className:"Test",...t},t.title),c={title:"Components/Lists/Testo",component:n,tags:["autodocs"],args:{title:"keke"}},e={args:{title:"Hello world!"}};var r,s,o;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  args: {
+    title: 'Hello world!'
+  }
+}`,...(o=(s=e.parameters)==null?void 0:s.docs)==null?void 0:o.source}}};const m=["Primary"];export{e as Primary,m as __namedExportsOrder,c as default};
