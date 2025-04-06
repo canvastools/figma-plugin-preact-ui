84b9332a8c5727662676827e1142ed4a529e8f58
Aleksei Sushkov <903434@gmail.com>
2025-04-06 19:16:34 +0100

    config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,21 +1,22 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: false },
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react-hooks/recommended', 'plugin:storybook/recommended', 'plugin:storybook/recommended'],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
+  env: { browser: true, es2020: true },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:storybook/recommended",
+    "plugin:storybook/recommended",
+  ],
+  ignorePatterns: ["dist", ".eslintrc.cjs", "esbuild.js", "node_modules", "storybook-static"],
+  parser: "@typescript-eslint/parser",
   parserOptions: {
-    project: './tsconfig.json'
+    project: "./tsconfig.json",
   },
-  plugins: ['react-refresh'],
-  rules: {
-    '@typescript-eslint/no-unused-vars': [
-      'error',
-      {
-        argsIgnorePattern: '^_',
-        varsIgnorePattern: '^_',
-        caughtErrorsIgnorePattern: '^_'
-      }
-    ]
-  }
+  plugins: ["react-refresh"],
+  overrides: [
+    {
+      files: ["**/*.ts", "**/*.tsx"],
+    },
+  ],
 }
--- esbuild.js
+++ esbuild.js
@@ -1,42 +1,46 @@
-/* eslint-disable */
+import fs from "fs"
+import esbuild from "esbuild"
+import { sassPlugin } from "esbuild-sass-plugin"
+import { typecheckPlugin } from "@jgoz/esbuild-plugin-typecheck"
 
-import fs from 'fs'
-import esbuild from 'esbuild'
-import { sassPlugin } from 'esbuild-sass-plugin'
-
-const isProduction = process.argv.includes('--prod')
+const isProduction = process.argv.includes("--prod")
 
 const renamePlugin = () => {
   return {
-    name: 'rename-plugin',
+    name: "rename-plugin",
     setup(build) {
       build.onEnd((result) => {
-        fs.rename('dist/index.js', 'dist/lib.js', () => {})
-        fs.rename('dist/index.css', 'dist/styles.css', () => {})
+        fs.rename("dist/index.js", "dist/lib.js", () => {})
+        fs.rename("dist/index.css", "dist/styles.css", () => {})
       })
-    }
+    },
   }
 }
 
 let ctx = await esbuild.context({
-  entryPoints: ['src/index.ts'],
-  outdir: 'dist/',
-  target: ['es2017'],
+  entryPoints: ["src/index.ts"],
+  outdir: "dist/",
+  target: ["es2020"],
   bundle: true,
   minify: isProduction,
-  logLevel: 'debug',
-  external: ['preact', 'preact/compat'],
-  format: 'esm',
-  plugins: [sassPlugin(), renamePlugin()]
+  logLevel: "debug",
+  external: ["preact", "preact/compat"],
+  format: "esm",
+  plugins: [
+    sassPlugin(),
+    renamePlugin(),
+    typecheckPlugin({
+      configFile: "./tsconfig.json",
+      watch: process.env.npm_config_watch,
+    }),
+  ],
 })
 
 if (process.env.npm_config_watch) {
   await ctx.watch()
-  console.log('Watching...')
+  console.log("Watching...")
 } else {
   await ctx.rebuild()
-  console.log('Build is ready!')
+  console.log("Build is ready!")
   process.exit(1)
 }
-
-/* eslint-enable */
