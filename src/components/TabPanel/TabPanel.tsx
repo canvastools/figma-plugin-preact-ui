e18601cf7ca6d74d71b001a27bda87124bc5d7ee
Aleksei Sushkov <903434@gmail.com>
2025-08-20 14:31:27 +0100

    TabPanel

---
+++ src/components/TabPanel/TabPanel.tsx
@@ -0,0 +1,41 @@
+import { Fragment } from "preact"
+
+import { bem, typedForwardRef } from "../../utils"
+
+import type { TabPanelProps } from "./TabPanel.types"
+import "./TabPanel.scss"
+
+import { useTabContext } from "../TabContext/TabContext"
+
+import { Text } from "../Text/Text"
+
+/* --- */
+
+const TabPanelComponent = (
+  { className, value, children, ...rest }: TabPanelProps,
+  ref: preact.Ref<HTMLDivElement>
+) => {
+  const { value: activeValue } = useTabContext()
+
+  if (value !== activeValue) return <Fragment />
+
+  const _className = bem("TabPanel", undefined, {
+    selected: value === activeValue,
+  })
+
+  return (
+    <div
+      className={[_className, className].join(" ").trim()}
+      ref={ref}
+      {...rest}
+    >
+      <Text variant="body" size="medium">
+        <div className="TabPanel__content">{children}</div>
+      </Text>
+    </div>
+  )
+}
+
+export const TabPanel = typedForwardRef<TabPanelProps, HTMLDivElement>(
+  TabPanelComponent
+)
---
+++ src/components/TabPanel/TabPanel.types.ts
@@ -0,0 +1,5 @@
+export interface TabPanelProps {
+  className?: string
+  value: string
+  children: preact.ComponentChildren
+}
