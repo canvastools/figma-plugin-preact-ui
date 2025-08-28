1bfe10fe413e370187673e34ee2d921a432afbde
Aleksei Sushkov <903434@gmail.com>
2025-08-28 20:45:58 +0100

    tabpanel fixes

--- src/components/TabPanel/TabPanel.stories.tsx
+++ src/components/TabPanel/TabPanel.stories.tsx
@@ -4,6 +4,7 @@ import { TabContext } from "../TabContext/TabContext"
 import { TabPanel } from "./TabPanel"
 import { TabList } from "../TabList/TabList"
 import { Tab } from "../Tab/Tab"
+import { FullWidth } from "components/Button/Button.stories"
 
 const meta: Meta<typeof TabPanel> = {
   title: "Components/TabPanel",
@@ -29,6 +30,11 @@ const meta: Meta<typeof TabPanel> = {
       },
       description: "Value of the tab panel for any mode.",
     },
+    fullHeight: {
+      control: { type: "boolean" },
+      description:
+        "Set the height to 100% to occupy the entire height in flex containers.",
+    },
     children: {
       table: {
         type: {
@@ -46,24 +52,35 @@ type Story = StoryObj<typeof TabPanel>
 export const Demo: Story = {
   args: {
     className: "",
+    fullHeight: false,
   },
   render: (args) => (
-    <TabContext defaultValue="tab-1">
-      <TabList>
-        <Tab value="tab-1">Tab 1</Tab>
-        <Tab value="tab-2">Tab 2</Tab>
-        <Tab value="tab-3">Tab 3</Tab>
-      </TabList>
-      <br />
-      <TabPanel {...args} value="tab-1">
-        Tab 1 Panel
-      </TabPanel>
-      <TabPanel {...args} value="tab-2">
-        Tab 2 Panel
-      </TabPanel>
-      <TabPanel {...args} value="tab-3">
-        Tab 3 Panel
-      </TabPanel>
-    </TabContext>
+    <div
+      className="sb-column sb-gap-16 sb-container"
+      style={{ width: 300, height: 300 }}
+    >
+      <TabContext defaultValue="tab-1">
+        <TabList>
+          <Tab value="tab-1">Tab 1</Tab>
+          <Tab value="tab-2">Tab 2</Tab>
+          <Tab value="tab-3">Tab 3</Tab>
+        </TabList>
+        <TabPanel {...args} value="tab-1">
+          <div style={{ backgroundColor: "yellow", height: "100%" }}>
+            Tab 1 Panel
+          </div>
+        </TabPanel>
+        <TabPanel {...args} value="tab-2">
+          <div style={{ backgroundColor: "yellow", height: "100%" }}>
+            Tab 2 Panel
+          </div>
+        </TabPanel>
+        <TabPanel {...args} value="tab-3">
+          <div style={{ backgroundColor: "yellow", height: "100%" }}>
+            Tab 3 Panel
+          </div>
+        </TabPanel>
+      </TabContext>
+    </div>
   ),
 }
