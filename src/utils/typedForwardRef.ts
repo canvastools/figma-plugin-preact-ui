c4fc18958cf01cf33ad4b31e9819b4cbb517fc16
Aleksei Sushkov <903434@gmail.com>
2025-09-21 23:34:25 +0100

    Update typedForwardRef.ts

--- src/utils/typedForwardRef.ts
+++ src/utils/typedForwardRef.ts
@@ -2,11 +2,11 @@ import { forwardRef } from "preact/compat"
 import type { Ref } from "preact"
 
 function typedForwardRef<Props, Element = unknown>(
-  render: (props: Props, ref: Ref<Element>) => preact.VNode
+  render: (props: Props, ref: Ref<Element>) => preact.VNode | null
 ) {
   return forwardRef(render) as (
     props: Props & { ref?: Ref<Element> }
-  ) => preact.VNode
+  ) => preact.VNode | null
 }
 
 export { typedForwardRef }
