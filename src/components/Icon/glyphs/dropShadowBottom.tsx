031a387165413558bfae74ec03a8ef4abdf83331
Aleksei Sushkov <903434@gmail.com>
2026-05-24 23:13:34 +0100

    icons

---
+++ src/components/Icon/glyphs/dropShadowBottom.tsx
@@ -0,0 +1,55 @@
+import { GlyphProps } from '../Icon.types'
+
+export const dropShadowBottom = ({ variant, size }: GlyphProps) => {
+  const combination = size + '_' + variant
+
+  switch (combination) {
+    case '16_default':
+      return (
+        <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path
+            d="M4.5 14C3.67157 14 3 13.3284 3 12.5L3 11L4 12H12L13 11V12.5C13 13.3284 12.3284 14 11.5 14H4.5Z"
+            fill="currentColor"
+            fill-opacity="0.3"
+          />
+          <path
+            d="M11.5 3C12.3284 3 13 3.67157 13 4.5V11.5C13 12.2767 12.4097 12.9154 11.6533 12.9922L11.5 13H4.5L4.34668 12.9922C3.64069 12.9205 3.07949 12.3593 3.00781 11.6533L3 11.5V4.5C3 3.67157 3.67157 3 4.5 3H11.5ZM4.5 4C4.22386 4 4 4.22386 4 4.5V11.5C4 11.7761 4.22386 12 4.5 12H11.5C11.7761 12 12 11.7761 12 11.5V4.5C12 4.22386 11.7761 4 11.5 4H4.5Z"
+            fill="currentColor"
+          />
+        </svg>
+      )
+
+    case '24_default':
+      return (
+        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path
+            d="M8 20C6.89543 20 6 19.1046 6 18V16L7 17H17L18 16V18C18 19.1046 17.1046 20 16 20H8Z"
+            fill="currentColor"
+            fill-opacity="0.3"
+          />
+          <path
+            d="M16.2041 6.01074C17.2128 6.113 18 6.96435 18 8V16L17.9893 16.2041C17.8938 17.1457 17.1457 17.8938 16.2041 17.9893L16 18H8C6.96435 18 6.113 17.2128 6.01074 16.2041L6 16V8C6 6.89543 6.89543 6 8 6H16L16.2041 6.01074ZM8 7C7.44772 7 7 7.44772 7 8V16C7 16.5523 7.44772 17 8 17H16C16.5523 17 17 16.5523 17 16V8C17 7.44772 16.5523 7 16 7H8Z"
+            fill="currentColor"
+          />
+        </svg>
+      )
+
+    case '24_upscaled':
+      return (
+        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path
+            d="M6 22C4.89543 22 4 21.1046 4 20V18L5 19H19L20 18V20C20 21.1046 19.1046 22 18 22H6Z"
+            fill="currentColor"
+            fill-opacity="0.3"
+          />
+          <path
+            d="M18 4C19.1046 4 20 4.89543 20 6V18C20 19.1046 19.1046 20 18 20H6C4.89543 20 4 19.1046 4 18V6C4 4.89543 4.89543 4 6 4H18ZM6 5C5.44772 5 5 5.44772 5 6V18C5 18.5523 5.44772 19 6 19H18C18.5523 19 19 18.5523 19 18V6C19 5.44772 18.5523 5 18 5H6Z"
+            fill="currentColor"
+          />
+        </svg>
+      )
+
+    default:
+      throw new Error(`dropShadowBottom icon error: Unsupported combination size=${size}, variant=${variant}`)
+  }
+}
---
+++ src/components/Icon/glyphs/innerShadowTop.tsx
@@ -0,0 +1,55 @@
+import { GlyphProps } from '../Icon.types'
+
+export const innerShadowTop = ({ variant, size }: GlyphProps) => {
+  const combination = size + '_' + variant
+
+  switch (combination) {
+    case '16_default':
+      return (
+        <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path
+            d="M12 4V7C12 6.44772 11.5523 6 11 6H5C4.44772 6 4 6.44772 4 7V4L12 4Z"
+            fill="currentColor"
+            fill-opacity="0.3"
+          />
+          <path
+            d="M11.5 3C12.3284 3 13 3.67157 13 4.5V11.5C13 12.2767 12.4097 12.9154 11.6533 12.9922L11.5 13H4.5L4.34668 12.9922C3.64069 12.9205 3.07949 12.3593 3.00781 11.6533L3 11.5V4.5C3 3.67157 3.67157 3 4.5 3H11.5ZM4.5 4C4.22386 4 4 4.22386 4 4.5V11.5C4 11.7761 4.22386 12 4.5 12H11.5C11.7761 12 12 11.7761 12 11.5V4.5C12 4.22386 11.7761 4 11.5 4H4.5Z"
+            fill="currentColor"
+          />
+        </svg>
+      )
+
+    case '24_default':
+      return (
+        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path
+            d="M17 7V10C17 9.44772 16.5523 9 16 9H8C7.44772 9 7 9.44772 7 10V7L17 7Z"
+            fill="currentColor"
+            fill-opacity="0.3"
+          />
+          <path
+            d="M16.2041 6.01074C17.2128 6.113 18 6.96435 18 8V16L17.9893 16.2041C17.8938 17.1457 17.1457 17.8938 16.2041 17.9893L16 18H8C6.96435 18 6.113 17.2128 6.01074 16.2041L6 16V8C6 6.89543 6.89543 6 8 6H16L16.2041 6.01074ZM8 7C7.44772 7 7 7.44772 7 8V16C7 16.5523 7.44772 17 8 17H16C16.5523 17 17 16.5523 17 16V8C17 7.44772 16.5523 7 16 7H8Z"
+            fill="currentColor"
+          />
+        </svg>
+      )
+
+    case '24_upscaled':
+      return (
+        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path
+            d="M19 5V8C19 7.44772 18.5523 7 18 7L6 7C5.44772 7 5 7.44772 5 8V5L19 5Z"
+            fill="currentColor"
+            fill-opacity="0.3"
+          />
+          <path
+            d="M18 4C19.1046 4 20 4.89543 20 6V18C20 19.1046 19.1046 20 18 20H6C4.89543 20 4 19.1046 4 18V6C4 4.89543 4.89543 4 6 4H18ZM6 5C5.44772 5 5 5.44772 5 6V18C5 18.5523 5.44772 19 6 19H18C18.5523 19 19 18.5523 19 18V6C19 5.44772 18.5523 5 18 5H6Z"
+            fill="currentColor"
+          />
+        </svg>
+      )
+
+    default:
+      throw new Error(`innerShadowTop icon error: Unsupported combination size=${size}, variant=${variant}`)
+  }
+}
