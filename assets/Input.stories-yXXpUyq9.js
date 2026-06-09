fafccd7f615660a3d2d50e84c86228bdbdd47b10
alexssh <alexssh@users.noreply.github.com>
2026-06-09 21:14:13 +0000

    deploy: 1485cd160a06644df092a15397854fa1116c4d0b

---
+++ assets/Input.stories-yXXpUyq9.js
@@ -0,0 +1,125 @@
+import{u as e,d as A}from"./hooks.module-6Sbq__eq.js";import{f as a}from"./index-B4E_jmCM.js";import{I as t}from"./Input-D2reW71y.js";import{T as j}from"./Text-BbQ_b1VF.js";import{I as p}from"./Icon-DqIclgYW.js";import{s as m}from"./search-BN53RESs.js";import{B as f}from"./ButtonIcon-DRpmjExh.js";import{l as g}from"./link-DL1y3n50.js";import{a as q}from"./TooltipContext-BiboncQ6.js";import"./preact.module-gH5CyEHP.js";import"./typedForwardRef-CtFJgl6m.js";import"./compat.module-CHmB9zKI.js";import"./TooltipContainer-CJCq4A9v.js";const z={parameters:{controls:{disable:!0},viewport:{defaultViewport:"large"},docs:{source:{language:"tsx",code:`
+<Input defaultValue="Hello World!" />
+`}}},render:()=>e("div",{className:"sb-column sb-width-300 sb-gap-16",children:e(t,{defaultValue:"Hello World!"})})},G={parameters:{controls:{disable:!0},viewport:{defaultViewport:"large"},docs:{source:{language:"tsx",code:`
+const [value, setValue] = useState("Hello World!")
+
+<Input
+  value={value}
+  onValueChange={(args) => setValue(args.value)}
+/>
+`}}},render:()=>{const[o,K]=A("Hello World!");return e("div",{className:"sb-column sb-width-300 sb-gap-16",children:[e(j,{children:["value: ",o]}),e(t,{value:o,onValueChange:_=>K(_.value)})]})}},J={parameters:{controls:{disable:!0},viewport:{defaultViewport:"large"},docs:{description:{story:"If there is no label, the component will be displayed in the default variant."},source:{language:"tsx",code:`
+<Input variant="default" />
+
+<Input variant="list" />
+`}}},render:()=>e("div",{className:"sb-column sb-width-300 sb-gap-40",children:[e(t,{defaultValue:"Hello World!",label:"Label",variant:"default"}),e(t,{defaultValue:"Hello World!",label:"Label",variant:"list"})]})},Q={parameters:{controls:{disable:!0},viewport:{defaultViewport:"large"},docs:{source:{language:"tsx",code:`
+<Input placeholder="Placeholder" />
+`}}},render:()=>e("div",{className:"sb-column sb-width-300 sb-gap-16",children:e(t,{placeholder:"Placeholder"})})},R={parameters:{controls:{disable:!0},viewport:{defaultViewport:"large"},docs:{source:{language:"tsx",code:`
+<Input disabled />
+`}}},render:()=>e("div",{className:"sb-column sb-width-300 sb-gap-16",children:[e(t,{disabled:!0}),e(t,{disabled:!0,placeholder:"Placeholder"}),e(t,{disabled:!0,defaultValue:"Default Value"})]})},X={parameters:{controls:{disable:!0},viewport:{defaultViewport:"large"},docs:{source:{language:"tsx",code:`
+<Input
+  prefix={
+    <Icon
+      glyph={search}
+      intentModifier="secondary"
+    />
+  }
+/>
+`}}},render:()=>e("div",{className:"sb-column sb-width-300 sb-gap-16",children:[e(t,{placeholder:"Input with prefix",prefix:e(p,{glyph:m,intentModifier:"secondary"})}),e(t,{placeholder:"Input with prefix (focusOnPrefix)",focusOnPrefix:!0,prefix:e(p,{glyph:m,intentModifier:"secondary"})})]})},Y={parameters:{controls:{disable:!0},viewport:{defaultViewport:"large"},docs:{source:{language:"tsx",code:`
+<Input
+  suffix={
+    <ButtonIcon 
+      icon={{ glyph: link }} 
+      translucent
+    />
+  }
+/>
+
+<Input
+  showSuffixOnHover
+  suffix={
+    <ButtonIcon 
+      icon={{ glyph: link }} 
+      translucent
+    />
+  }
+/>
+`}}},render:()=>e("div",{className:"sb-column sb-width-300 sb-gap-16",children:[e(t,{placeholder:"Input with suffix",suffix:e(f,{icon:{glyph:g},translucent:!0})}),e(t,{placeholder:"Show suffix on hover",showSuffixOnHover:!0,suffix:e(f,{icon:{glyph:g},translucent:!0})})]})},ce={title:"Components/Input",component:t,tags:["autodocs"],argTypes:{id:{control:{type:"text"},table:{type:{summary:"string"}}},className:{control:{type:"text"}},variant:{control:{type:"radio"},options:["default","list"],defaultValue:{summary:"default"}},label:{control:{type:"text"}},placeholder:{control:{type:"text"}},type:{control:{type:"radio"},options:["text","number"],defaultValue:{summary:"text"}},defaultValue:{control:{type:"text"},description:"Value for uncontrolled state."},value:{control:{disable:!0},description:"Value for controlled state.",table:{type:{summary:"string"}}},ghost:{control:{type:"boolean"},defaultValue:{summary:!1}},grouped:{control:{type:"radio"},options:[void 0,"first","last","middle"]},error:{control:{type:"boolean"},defaultValue:{summary:!1}},disabled:{control:{type:"boolean"},defaultValue:{summary:!1}},prefix:{control:{disable:!0},description:"Element displayed before value.",table:{type:{summary:"preact.ComponentChildren"}}},suffix:{control:{disable:!0},description:"Element displayed after value.",table:{type:{summary:"preact.ComponentChildren"}}},showSuffixOnHover:{control:{type:"boolean"},defaultValue:{summary:!1}},focusOnDoubleClick:{control:{type:"boolean"},defaultValue:{summary:!1}},focusOnPrefix:{control:{type:"boolean"},defaultValue:{summary:!1}},minLength:{control:{type:"number"},defaultValue:{summary:0},description:"Minimum length of the input value."},maxLength:{control:{type:"number"},defaultValue:{summary:1/0},description:"Maximum length of the input value."},tooltip:{control:{type:"text"},description:"Tooltip content.",table:{type:{summary:"preact.ComponentChildren"}}},autoFocus:{control:{type:"boolean"},defaultValue:{summary:!1}},selectOnFocus:{control:{type:"boolean"},defaultValue:{summary:!1},description:"Select all text when the input receives focus."},maxWidth:{control:{type:"number"},description:"Maximum width of the input (excluding label). Only applicable when variant is `list`.",table:{type:{summary:"number | string"}}},tabIndex:{control:{type:"number"},description:"Tab order of the focusable element (input, or display container when focusOnDoubleClick is enabled). Applied to a single tab stop only. Omit to keep the default.",table:{type:{summary:"number"}}},onValueChange:{table:{type:{summary:"(args) => void",detail:`
+args: { 
+  event: MouseEvent
+  value: string
+}
+          `}}},onBlur:{table:{type:{summary:"(args) => void",detail:`
+args: { 
+  event: FocusEvent
+  value: string
+}
+          `}}},onFocus:{table:{type:{summary:"(args) => void",detail:`
+args: { 
+  event: FocusEvent
+  value: string
+}
+          `}}},onKeyDown:{table:{type:{summary:"(args) => void",detail:`
+args: { 
+  event: KeyboardEvent
+  value: string
+}
+          `}}}}},r={tags:["!autodocs"],args:{id:void 0,className:"",variant:"default",label:"Label",type:"text",placeholder:"Placeholder",defaultValue:"",ghost:!1,grouped:void 0,error:!1,disabled:!1,showSuffixOnHover:!1,focusOnDoubleClick:!1,tooltip:"Input tooltip",minLength:0,maxLength:9999,autoFocus:!1,selectOnFocus:!1,maxWidth:void 0,onValueChange:a(),onBlur:a(),onFocus:a(),onKeyDown:a()},parameters:{viewport:{defaultViewport:"large"},docs:{source:{language:"tsx",code:`
+<Input {...args} />
+
+// Use TooltipContext to make tooltips work
+
+<TooltipContext>
+  <Input {...args} />
+</TooltipContext>
+`}}},render:o=>e("div",{className:"sb-column sb-width-300",children:e(q,{children:e(t,{...o})})})},l=z,n=G,s=J,u=Q,i=R,d=X,c=Y;var b,y,h;r.parameters={...r.parameters,docs:{...(b=r.parameters)==null?void 0:b.docs,source:{originalSource:`{
+  tags: ['!autodocs'],
+  args: {
+    id: undefined,
+    className: '',
+    variant: 'default',
+    label: 'Label',
+    type: 'text',
+    placeholder: 'Placeholder',
+    defaultValue: '',
+    ghost: false,
+    grouped: undefined,
+    error: false,
+    disabled: false,
+    showSuffixOnHover: false,
+    focusOnDoubleClick: false,
+    tooltip: 'Input tooltip',
+    minLength: 0,
+    maxLength: 9999,
+    autoFocus: false,
+    selectOnFocus: false,
+    maxWidth: undefined,
+    onValueChange: fn(),
+    onBlur: fn(),
+    onFocus: fn(),
+    onKeyDown: fn()
+  },
+  parameters: {
+    viewport: {
+      defaultViewport: 'large'
+    },
+    docs: {
+      source: {
+        language: 'tsx',
+        code: \`
+<Input {...args} />
+
+// Use TooltipContext to make tooltips work
+
+<TooltipContext>
+  <Input {...args} />
+</TooltipContext>
+\`
+      }
+    }
+  },
+  render: args => <div className="sb-column sb-width-300">
+      <TooltipContext>
+        <Input {...args} />
+      </TooltipContext>
+    </div>
+}`,...(h=(y=r.parameters)==null?void 0:y.docs)==null?void 0:h.source}}};var x,v,w;l.parameters={...l.parameters,docs:{...(x=l.parameters)==null?void 0:x.docs,source:{originalSource:"UncontrolledStory",...(w=(v=l.parameters)==null?void 0:v.docs)==null?void 0:w.source}}};var V,S,I;n.parameters={...n.parameters,docs:{...(V=n.parameters)==null?void 0:V.docs,source:{originalSource:"ControlledStory",...(I=(S=n.parameters)==null?void 0:S.docs)==null?void 0:I.source}}};var C,O,P;s.parameters={...s.parameters,docs:{...(C=s.parameters)==null?void 0:C.docs,source:{originalSource:"VariantStory",...(P=(O=s.parameters)==null?void 0:O.docs)==null?void 0:P.source}}};var D,T,k;u.parameters={...u.parameters,docs:{...(D=u.parameters)==null?void 0:D.docs,source:{originalSource:"PlaceholderStory",...(k=(T=u.parameters)==null?void 0:T.docs)==null?void 0:k.source}}};var N,F,H;i.parameters={...i.parameters,docs:{...(N=i.parameters)==null?void 0:N.docs,source:{originalSource:"DisabledStory",...(H=(F=i.parameters)==null?void 0:F.docs)==null?void 0:H.source}}};var L,W,B;d.parameters={...d.parameters,docs:{...(L=d.parameters)==null?void 0:L.docs,source:{originalSource:"PrefixStory",...(B=(W=d.parameters)==null?void 0:W.docs)==null?void 0:B.source}}};var E,M,U;c.parameters={...c.parameters,docs:{...(E=c.parameters)==null?void 0:E.docs,source:{originalSource:"SuffixStory",...(U=(M=c.parameters)==null?void 0:M.docs)==null?void 0:U.source}}};const pe=["Demo","Uncontrolled","Controlled","Variant","Placeholder","Disabled","Prefix","Suffix"];export{n as Controlled,r as Demo,i as Disabled,u as Placeholder,d as Prefix,c as Suffix,l as Uncontrolled,s as Variant,pe as __namedExportsOrder,ce as default};
---
+++ assets/PopoverContext.stories-DsVZkrFx.js
@@ -0,0 +1,110 @@
+import{d as v,A as h,u as o}from"./hooks.module-6Sbq__eq.js";import{P as c,u as C}from"./PopoverContext-BKF6rnxf.js";import{B as P}from"./Button-Cd6Vogte.js";import{P as R}from"./PopoverContainer-DIFx4uVt.js";import{P as b}from"./PopoverHeader-DTeNmMC1.js";import{S as O}from"./Section-f0GkCzPh.js";import{T as x}from"./Text-BbQ_b1VF.js";import{O as y}from"./TooltipContext-BiboncQ6.js";import"./preact.module-gH5CyEHP.js";import"./typedForwardRef-CtFJgl6m.js";import"./compat.module-CHmB9zKI.js";import"./ButtonIcon-DRpmjExh.js";import"./Icon-DqIclgYW.js";import"./close-DCtOkKcs.js";import"./TooltipContainer-CJCq4A9v.js";const S={parameters:{controls:{disable:!0},viewport:{defaultViewport:"large"},docs:{source:{language:"tsx",code:`
+const PopoverContent = () => {
+  const context = usePopoverContext()
+  if (!context) return null
+
+  return (
+    <OverlayPositioner
+      anchorRef={context.anchorRef}
+      open={context.open}
+      placement="bottom-left"
+      offsetX={4}
+      offsetY={4}
+      offsetEdge={16}
+      draggable={true}
+      onClose={() => context.setOpen(false))}
+    >
+      <PopoverContainer width={300}>
+
+        <PopoverHeader onClose={() => context.setOpen(false)}>
+          Header
+        </PopoverHeader>
+
+        <Section>
+          {children}
+        </Section>
+
+      </PopoverContainer>
+    </OverlayPositioner>
+  )
+}
+
+const ControlledPopover = () => {
+  const [open, setOpen] = useState(args.open)
+  const triggerRef = useRef(null)
+
+  return (
+    <div>
+      <Button
+        ref={triggerRef}
+        onClick={() => setOpen(true)}
+      >
+        {children}
+      </Button>
+      
+      <PopoverContext
+        triggerRef={triggerRef}
+        anchorRef={triggerRef}
+        open={open}
+        setOpen={setOpen}
+      >
+        <PopoverContent />
+      </PopoverContext>
+    </div>
+  )
+}
+
+
+`}}},render:l=>{const[r,s]=v(l.open),n=h(null);return o("div",{className:"sb-column sb-width-full",children:[o(P,{ref:n,onClick:()=>s(!0),children:"Open Popover"}),o(c,{triggerRef:n,anchorRef:n,open:r,setOpen:s,children:o(()=>{const e=C();return e?o(y,{anchorRef:e==null?void 0:e.anchorRef,open:e.open,placement:"bottom-left",offsetX:4,offsetY:4,offsetEdge:16,draggable:!0,onClose:()=>{var t;return(t=e.setOpen)==null?void 0:t.call(e,!r)},children:o(R,{width:300,children:[o(b,{onClose:()=>{var t;return(t=e.setOpen)==null?void 0:t.call(e,!r)},children:"Header"}),o(O,{children:o(x,{children:"Content"})})]})}):null},{})})]})}},F={title:"Components/PopoverContext",component:c,tags:["autodocs"],parameters:{docs:{description:{component:"A context provider that manages open state, focus, and keyboard navigation of Popover-related components."}}},argTypes:{triggerRef:{control:{disable:!0},description:"Ref to the trigger element.",table:{type:{summary:"preact.RefObject"}}},anchorRef:{control:{disable:!0},description:"Ref to the anchor element. If not provided, the triggerRef will be used",table:{type:{summary:"preact.RefObject"}}},open:{control:{type:"boolean"},description:"State for controlled state."},setOpen:{control:{disable:!0},description:"Function to set the open state.",table:{type:{summary:"(open: boolean) => void"}}},children:{control:{disable:!0},description:"<strong>*</strong>",table:{type:{summary:"preact.ComponentChildren"}}},usePopoverContext:{description:"Hook to access the context.",table:{type:{summary:"Props",detail:`
+  {
+    triggerRef: RefObject | null
+    anchorRef: RefObject | null
+    open: boolean
+    setOpen: (open: boolean) => void
+  }
+  `}}}}},a={tags:["!autodocs"],args:{open:!1},parameters:{viewport:{defaultViewport:"large"},docs:{source:{language:"tsx",code:`
+<PopoverContext {...args}>{children}</PopoverContext>
+`}}},render:l=>{const[r,s]=v(l.open),n=h(null);return o("div",{className:"sb-column sb-width-full",children:[o(P,{ref:n,onClick:()=>s(!0),children:"Open Popover"}),o(c,{triggerRef:n,anchorRef:n,open:r,setOpen:s,children:o(()=>{const e=C();return e?o(y,{anchorRef:e==null?void 0:e.anchorRef,open:e.open,placement:"bottom-left",offsetX:4,offsetY:4,offsetEdge:16,draggable:!0,onClose:()=>{var t;return(t=e.setOpen)==null?void 0:t.call(e,!r)},children:o(R,{width:300,children:[o(b,{onClose:()=>{var t;return(t=e.setOpen)==null?void 0:t.call(e,!r)},children:"Header"}),o(O,{children:o(x,{children:"Content"})})]})}):null},{})})]})}},p=S;var i,d,f;a.parameters={...a.parameters,docs:{...(i=a.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  tags: ['!autodocs'],
+  args: {
+    open: false
+  },
+  parameters: {
+    viewport: {
+      defaultViewport: 'large'
+    },
+    docs: {
+      source: {
+        language: 'tsx',
+        code: \`
+<PopoverContext {...args}>{children}</PopoverContext>
+\`
+      }
+    }
+  },
+  render: args => {
+    // @ts-expect-error Storybook control value
+    const [open, setOpen] = useState(args.open);
+    const triggerRef = useRef<HTMLButtonElement | null>(null);
+    const PopoverContent = () => {
+      const context = usePopoverContext();
+      if (!context) return null;
+      return <OverlayPositioner anchorRef={context?.anchorRef as preact.RefObject<HTMLElement>} open={context.open} placement="bottom-left" offsetX={4} offsetY={4} offsetEdge={16} draggable={true} onClose={() => context.setOpen?.(!open)}>
+          <PopoverContainer width={300}>
+            <PopoverHeader onClose={() => context.setOpen?.(!open)}>Header</PopoverHeader>
+            <Section>
+              <Text>Content</Text>
+            </Section>
+          </PopoverContainer>
+        </OverlayPositioner>;
+    };
+    return <div className="sb-column sb-width-full">
+        <Button ref={triggerRef} onClick={() => setOpen(true)}>
+          Open Popover
+        </Button>
+        <PopoverContext triggerRef={triggerRef} anchorRef={triggerRef} open={open} setOpen={setOpen}>
+          <PopoverContent />
+        </PopoverContext>
+      </div>;
+  }
+}`,...(f=(d=a.parameters)==null?void 0:d.docs)==null?void 0:f.source}}};var u,m,g;p.parameters={...p.parameters,docs:{...(u=p.parameters)==null?void 0:u.docs,source:{originalSource:"ControlledStory",...(g=(m=p.parameters)==null?void 0:m.docs)==null?void 0:g.source}}};const I=["Demo","Controlled"];export{p as Controlled,a as Demo,I as __namedExportsOrder,F as default};
