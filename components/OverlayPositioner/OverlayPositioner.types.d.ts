973b1bc5cb5966b7454a675dbca90a0f77676eae
alexssh <alexssh@users.noreply.github.com>
2026-06-09 21:32:50 +0000

    deploy: fb835b1d39cc897257d9c9d03155f9fd84119455

--- components/OverlayPositioner/OverlayPositioner.types.d.ts
+++ components/OverlayPositioner/OverlayPositioner.types.d.ts
@@ -14,6 +14,7 @@ export interface OverlayPositionerProps {
     offsetEdge?: number;
     closeOnClickOutside?: boolean;
     autoReposition?: boolean;
+    constrainHeight?: boolean;
     onOpen?: () => void;
     onClose?: () => void;
     children: preact.ComponentChildren;
