41c60e21866a80d81b4f9c58398a0a7f5fedcb22
Aleksei Sushkov <903434@gmail.com>
2026-07-16 00:19:48 +0100

    Icon chevron fix 2

--- src/components/Calendar/Calendar.tsx
+++ src/components/Calendar/Calendar.tsx
@@ -69,10 +69,10 @@ const CalendarComponent = (
         maxDate={maxDate}
         minDetail={minDetail}
         maxDetail={maxDetail}
-        prevLabel={<Icon glyph={chevronRight} variant="downscaled" />}
-        prev2Label={<Icon glyph={chevronDoubleRight} variant="downscaled" />}
-        nextLabel={<Icon glyph={chevronLeft} variant="downscaled" />}
-        next2Label={<Icon glyph={chevronDoubleLeft} variant="downscaled" />}
+        prevLabel={<Icon glyph={chevronLeft} variant="downscaled" />}
+        prev2Label={<Icon glyph={chevronDoubleLeft} variant="downscaled" />}
+        nextLabel={<Icon glyph={chevronRight} variant="downscaled" />}
+        next2Label={<Icon glyph={chevronDoubleRight} variant="downscaled" />}
         onChange={(e) => {
           onDateChange?.({ date: e })
         }}
